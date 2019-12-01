81504a6d1bb5353c771e81a4ea496adae0f4a7bd
Sofía <sofianeirag@gmail.com>
2019-12-01 16:42:52 +0100

    Otro commit más.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,11 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world, this is me again!");
+		System.out.println("Hello world!");
+	}
+	
+	private void prueba() {
+		
 	}
 
 }
